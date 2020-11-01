76a800bb6f96ec10abe729f8012cab84b79fae85
romchambe <chamberomain@gmail.com>
2020-11-01 16:44:49 +0100

    satisfy linter'

--- src/controllers/App.tsx
+++ src/controllers/App.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import "../styles/tailwind.output.css"
-import { DaylightBackground } from "../uicomponents/DaylightBackground"
+import { Session } from "./Session"
 
-function App() {
+const App: React.FunctionComponent<null> = () => {
   return (
     <div className="h-screen w-full flex bg-blue">
-      <DaylightBackground duration={180000} timeToEnd={180000}>
-        <div className="rounded shadow bg-pale max-w-xs">coucou</div>
-      </DaylightBackground>
+      <Session></Session>
     </div>
   )
 }
---
+++ src/controllers/Session.tsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+import { DaylightBackground } from "../uicomponents/DaylightBackground"
+
+interface SessionState {
+  active: boolean
+  timer: number
+  duration: number
+}
+
+export class Session extends Component<{}, SessionState> {
+  timer: null | NodeJS.Timeout = null
+  constructor(props: {}) {
+    super(props)
+    this.state = { active: true, timer: 0, duration: 180000 }
+  }
+
+  componentDidMount(): void {
+    window.addEventListener("load", this.startTimer)
+    window.addEventListener("focus", this.onFocus)
+  }
+
+  startTimer: () => void = () => {
+    this.timer = setInterval(
+      () =>
+        this.setState((prevState) => {
+          return { timer: prevState.timer + 1000 }
+        }),
+      1000
+    )
+  }
+
+  stopTimer: () => void = () => {
+    if (!!this.timer) {
+      clearInterval(this.timer)
+    }
+  }
+
+  onFocus: () => void = () => {
+    if (!this.timer) {
+      this.startTimer()
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <DaylightBackground
+        duration={this.state.duration}
+        timeToEnd={this.state.duration - this.state.timer}
+      >
+        {this.props.children}
+      </DaylightBackground>
+    )
+  }
+}
