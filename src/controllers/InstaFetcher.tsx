da827f9768e218195de52e8f3b8be91820ba4adb
romchambe <chamberomain@gmail.com>
2020-11-07 09:47:17 +0100

    put insta fetch functions in utils file

--- src/controllers/InstaFetcher.tsx
+++ src/controllers/InstaFetcher.tsx
@@ -1,6 +1,11 @@
 import { Component } from "react"
 import miniget from "miniget"
 import { SessionContext } from "../contexts/SessionContext"
+import {
+  InstaImage,
+  parseHtml,
+  extractPostsData,
+} from "../utils/fetchHashtag"
 
 interface Props {
   hashtag: string
@@ -16,13 +21,6 @@ interface State {
 export interface PostsList {
   posts: Post[]
 }
-
-interface InstaImage {
-  src: string
-  config_width: number
-  config_height: number
-}
-
 export interface Post {
   images: InstaImage
   timestamp: number
@@ -55,82 +53,5 @@ export class InstaFetcher extends Component<Props, State> {
 
   render(): JSX.Element {
     return this.props.render(this.state)
-  }
-}
-
-function parseHtml(data: string): unknown {
-  const index = matchOrThrow(data, "entry_data")
-
-  const dataObj = data.substring(index)
-
-  const targetObject = ["{"]
-
-  try {
-    let charCounter = 0
-    let closureCount = 1
-
-    const openClosure = dataObj.substring(matchOrThrow(dataObj, "{") + 1)
-    const chars = openClosure.split("")
-
-    while (closureCount > 0) {
-      if (chars[charCounter] === "{") {
-        closureCount += 1
-      }
-
-      if (chars[charCounter] === "}") {
-        closureCount -= 1
-      }
-
-      if (closureCount > 0) {
-        targetObject.push(chars[charCounter])
-      }
-
-      charCounter += 1
-    }
-    targetObject.push("}")
-  } catch (error) {
-    console.error(error)
-  }
-
-  return JSON.parse(targetObject.join(""))
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function extractPostsData(rawPosts: any): Post[] {
-  let posts =
-    rawPosts.TagPage[0].graphql.hashtag.edge_hashtag_to_media.edges
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  posts = posts.map((rawPost: any) => {
-    const { node } = rawPost
-    const { id } = node
-    const timestamp = node.taken_at_timestamp
-    let description = ""
-    try {
-      description = node.edge_media_to_caption.edges[0].node.text
-    } catch (error) {
-      console.error(error, node)
-    }
-
-    const images = node.thumbnail_resources.reduce(
-      (prev: InstaImage, current: InstaImage) => {
-        return prev.config_width < current.config_width ? current : prev
-      }
-    )
-
-    return { timestamp, images, description, id }
-  })
-
-  posts.sort((a: Post, b: Post) => b.timestamp - a.timestamp)
-  return posts
-}
-
-function matchOrThrow(data: string, target: string) {
-  const match = data.match(target)
-
-  if (!!match && !!match.index) {
-    return match.index
-  } else {
-    throw new Error("no data")
   }
 }
