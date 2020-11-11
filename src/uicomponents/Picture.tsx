eea1fa929f0aff13d6692fb66b16393eff19c70d
romchambe <chamberomain@gmail.com>
2020-11-11 15:53:13 +0100

    ignore tailwind output

--- src/uicomponents/Picture.tsx
+++ src/uicomponents/Picture.tsx
@@ -18,7 +18,7 @@ export const Picture: React.FunctionComponent<Post> = (props) => {
       />
       <div
         className="font-yogasanspro py-3 px-3 text-md overflow-hidden"
-        style={{ maxHeight: "8.5rem" }}
+        style={{ maxHeight: "8.5rem", letterSpacing: -0.35 }}
       >
         {props.description}
       </div>
